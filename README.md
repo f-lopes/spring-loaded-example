dee594f5e01f9b58661b1bd1e86556a43d528f72
Florian Lopes <florian.lopes@outlook.com>
2015-06-07 18:53:39 +0200

    Add project files

---
+++ README.md
@@ -0,0 +1,3 @@
+Spring Loaded example using Spring Boot
+
+This sample contains a controller to demonstrate the reload of Spring MVC controllers using Spring Loaded
---
+++ src/main/java/fr/ippon/spring/loaded/SpringLoadedExampleApplication.java
@@ -0,0 +1,12 @@
+package fr.ippon.spring.loaded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoadedExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLoadedExampleApplication.class, args);
+    }
+}
